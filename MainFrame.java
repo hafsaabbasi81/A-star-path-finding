99985d13b23e0e3e85d8c8dbe4f48e5714475122
Hafsa Abbasi <74056206+hafsaabbasi81@users.noreply.github.com>
2021-07-27 21:56:30 +0500

    Create MainFrame.java

---
+++ MainFrame.java
@@ -0,0 +1,13 @@
+package pathfinding.ui;
+
+
+import javax.swing.JFrame;
+
+public class MainFrame extends JFrame {
+//exit from frame
+    public MainFrame(){
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(null);
+    }
+    
+}
